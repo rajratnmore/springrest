b72d9208e4aca91b460975f2c59c0e81d730a164
Rajratn More <rajmorecsn@gmail.com>
2023-07-22 15:21:16 +0530

    current commit

--- springrest/src/main/java/com/springrest/springrest/controller/MyController.java
+++ springrest/src/main/java/com/springrest/springrest/controller/MyController.java
@@ -8,43 +8,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springrest.springrest.entities.Course;
 import com.springrest.springrest.services.CourseService;
 
 @RestController
+@RequestMapping("/courses")
 public class MyController {
 	
 	@Autowired
 	private CourseService courseService;
 	
 	// Get all courses
-	@GetMapping("/courses")
+	@GetMapping("/")
 	public List<Course> getCourses(){
 		return this.courseService.getCourses();
 	}
 	
 	// Get single course
-	@GetMapping("/course/{courseId}")
+	@GetMapping("/{courseId}")
 	public Course getCourse(@PathVariable String courseId) {
 		return this.courseService.getCourse(Long.parseLong(courseId));		
 	}
 	
 	// Add course
-	@PostMapping("course")
+	@PostMapping("/")
 	public Course addCourse(@RequestBody Course course) {
 		return this.courseService.addCourse(course);
 	}
 	
 	// Update course
-	@PutMapping("course")
+	@PutMapping("/")
 	public Course updateCourse(@RequestBody Course course) {
 		return this.courseService.updateCourse(course);
 	}
 	
 	// Delete course
-	@DeleteMapping("/course/{courseId}")
+	@DeleteMapping("/{courseId}")
 	public void deleteCourse(@PathVariable String courseId) {
 		this.courseService.deleteCourse(Long.parseLong(courseId));
 	}
